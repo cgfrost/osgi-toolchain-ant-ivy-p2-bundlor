ccf1bc39bb79e4411d4184c6c84c3d49ee3beef0
Christopher Frost <frostc@vmware.com>
2013-03-27 00:06:38 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 osgi-toolchain-ant-ivy-p2-bundlor
 =================================
+
+For full details please see: https://github.com/cgfrost/osgi-toolchain-ant-ivy-p2-bundlor
---
+++ demo.bundle.two/src/main/java/net/codersbrew/ant/demo/bundle/two/IndexServlet.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2008, 2011 VMware Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   VMware Inc. - initial contribution
+ *******************************************************************************/
+package net.codersbrew.ant.demo.bundle.two;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * A very simple servlet to redirect requests to the root servlet path to the overview page
+ *
+ */
+public class IndexServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.sendRedirect(Activator.contextPath + "/content/home");
+	}
+	
+}
